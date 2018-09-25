fd71fb46e73d18d01beb225b5ed49898ef627406
Andreas Trøstrup <andreastrstrup@gmail.com>
2018-09-25 10:44:26 +0200

    gitignore

--- src/Test.java
+++ src/Test.java
@@ -2,5 +2,11 @@
 public class Test {
 	public static void main(String[] args) {
 		System.out.println("Hello World!");
+		String adrian = "noob";
+		boolean isNoob;
+		if(adrian.equals("noob")) {
+			isNoob = true;
+			System.out.println(isNoob);
+		}
 	}
 }
