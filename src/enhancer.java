d6fecf652621600e7314409ddd822f6e47181fd2
Andreas Trøstrup <andreastrstrup@gmail.com>
2018-09-29 12:57:52 +0200

    Commentary-revision #4.. Nu på danskaddall!

--- src/enhancer.java
+++ src/enhancer.java
@@ -1,10 +1,10 @@
-//this file is the "enhancer-frontend", where i will get the file to read, and then enhance that.
-//everything back-end takes place in enhancerEngine.java
+// Denne fil er "front-enden", det er her alt skal køres fra
+// Alt den rå kode ligger i engine.java
 public class enhancer {
 	public static void main(String[] args) {
-		String input = FileScanner.readFromFile("UglyDuckling"); 					//set the filescanner.readfromfile method return value
-		String enhancedFinal = enhancerEngine.enhance(input);						//set the string enhancedFinal to equal the return value of the enhance method on the input
-		System.out.println(enhancedFinal);
-	//	FileScanner.writeToFile(enhancedFinal,"UglyDuckling.rtf"); 					//write the enhanced final
+		String input = FileScanner.readFromFile("UglyDuckling"); 					// Få inputtet fra readFromFile methoden
+		String enhancedFinal = enhancerEngine.enhance(input);						// set enhancedFinal stringen til at være lig med den enhancede version af inputtet
+		System.out.println(enhancedFinal);											// print den enhancede verison (for testing)
+	//	FileScanner.writeToFile(enhancedFinal,"UglyDuckling.rtf"); 					// skriv den enhancede version til en fil
 	}
 }
--- src/enhancerEngine.java
+++ src/enhancerEngine.java
@@ -2,62 +2,60 @@ import java.io.*;
 import java.util.regex.Pattern;
 
 @SuppressWarnings("unused")
-//this file is the back-end enhancer, run it from the enhancer.java class
+// Denne fil er "back-enden", den skal køres fra enhancer.java
 public class enhancerEngine {
 
 	public static String format(String input) {
-																						// first, this part will split up the read file by every "."
-																						// then it will add a line switch where it finds it.
-		String out = ""; 																// init the string to not get a nullpointerexception
-		String[] arr = input.split("(?<=[.])");										    // split the input string after every period (?<= means look behind)
-		for (String s : arr) { 															// your avg for-each loop
-			if (s.charAt(0) == ' ') { 													// if the first character is a space..
-				s = s.substring(1) + "\n"; 												// set the string to equal everything from the second character + a newline
+																											// Formateringsmetoden. Først splitter den ved hvert punktum, og så sætter den et linjeskift ind.
+		String out = ""; 																					// Initliaze output string, ellers får man en NullPointerExc.
+		String[] arr = input.split("(?<=[.])");										   						// Split input-stringen efter hvert punktum (bruger en RegEx, ?<= der betyder "kig bag/look behind"
+		for (String s : arr) { 																				// Et for each loop
+			if (s.charAt(0) == ' ') { 																		// Hvis den første char er et space..
+				s = s.substring(1) + "\n"; 																	// Sæt stringen til at være lig med alt efter mellemrummet, plus et linjeskift
 			}
-			out += s + "\n"; 															// set output string to equal out + s and a newline
+			out += s + "\n"; 																				// Sæt output string til at være lig med s og linjeskift
 		}
-																						// this part ensures that a space will be input every time two quotation marks are next to each other																				
-		StringBuilder b = new StringBuilder(out); 										// create StringBuilder object
-		for (int i = out.length() - 1; i >= 0; i--) { 									// for loop, counts downwards, because else the indices will be messed, bc we add something to the string. i equals length-1 bc of 0-indexing (if length is 100, max index is 99)																									
-			if ((out.charAt(i) == '\"') && (out.charAt(i + 1) == '\"')) {				// if the char at index i AND the char at i + 1 = "...																
-				b.insert(i + 1, " "); 													// use stringbuilder to insert a space
+																											// Denne del indsætter et mellemrum hvis der to citationstegn ved siden af hinanden ("Hej!""Hejsa"!)																			
+		StringBuilder b = new StringBuilder(out); 															// Lav StringBuilder objektet, der skal gøre arbejdet nemt
+		for (int i = out.length() - 1; i >= 0; i--) { 														// For-løkke der tæller baglæns, ellers får vi problemer, da længden bliver ændret når vi tilføjer noget. i er length()-1, pga 0-indexing,. (hvis længden er 100, er max index 99)																								
+			if ((out.charAt(i) == '\"') && (out.charAt(i + 1) == '\"')) {									// hvis char at i OG char at i+1 = "...														
+				b.insert(i + 1, " "); 																		// brug StringBuilder til at indsætte et mellemrum
 			}
 		}
-		return b.toString(); 															// return the final text as a string
+		return b.toString(); 																				// Return den endelige StringBuilder value som en string
 
 	}
 
-	public static String replace(String input) { 										// this method replaces the chosen words (opg 2)
-		String out = "";
-		String test = "ælling";
-		String[][] multi = { { "Ælling", "Grisling" }, { "And", "Gris" }, { "Meget", "XXX" } };
-		String[] lines = input.split("\n");
+	public static String replace(String input) { 															// Replacer-methoden. (opg 2)
+		String out = "";																					// Samme som i format, NullPointerExc.
+		String[][] r = { { "Ælling", "Grisling" }, { "And", "Gris" } };										// Dette er et 2-layered array, der indeholder ordene der skal replaces, og det, ordet skal replaces til.
+		String[] lines = input.split("\n");																	// Split inputtet op i linjer (split ved hvert linjeskift)
 
-		for (String line : lines) {
-			String[] words = line.split(" ");
-			for (String word : words) {
-				for (int i = 0; i < multi.length; i++) {
+		for (String line : lines) {																			// for each loop, itererer over hver linje
+			String[] words = line.split(" ");																// Split hver linje op i ord, (ved hvert Space)
+			for (String word : words) {																		// nyt for each loop, itererer over hvert ord.
+				for (int i = 0; i < r.length; i++) {														// nyt for loop..... bruges til at iterere over hvert ord i replacer-arrayet.
 
-					if (word.matches("(.*)" + multi[i][0].toLowerCase() + "(.*)")) {
-						word = word.replace(multi[i][0].toLowerCase(), multi[i][1].toLowerCase());
-					} else if (word.matches("(.*)" + multi[i][0] + "(.*)")) {
-						word = word.replaceAll(multi[i][0], multi[i][1]);
+					if (word.matches("(.*)" + r[i][0].toLowerCase() + "(.*)")) {							// Hvis ordet matcher det første ord i "i"-subarrayet, (i lowercase). RegEx (.*) betyder ethvert symbol der optræder mere end 0 gange
+						word = word.replace(r[i][0].toLowerCase(), r[i][1].toLowerCase());					// erstat ordet med det næste ord i subarrayet (dens replacer)
+					} else if (word.matches("(.*)" + r[i][0] + "(.*)")) {								 	// ellers, hvis ordet matcher versionen af ordet som har stort forbogstav..
+						word = word.replace(r[i][0], r[i][1]);												// erstat ordet, med versionen der har stort forbogstav
 					}
-
+					
 				}
-				out += word + " ";
+				out += word + " ";																			// Link alle ordene sammen i output-stringen
 			}
 		}
-		return out;
+		return out;																							// return output-stringen												
 	}
 
-	public static String enhance(String input) {
-		String replaced = replace(input);
-		String enhanced = format(replaced);
-		return enhanced;
+	public static String enhance(String input) {															// Vores primære "runner" method. Det er denne, der skal calles
+		String replaced = replace(input);																	// Først, erstat ordene i indputtet
+		String enhanced = format(replaced);																	// Formater så den erstatte version
+		return enhanced;																					// Return den endelige forbedrede version
 	}
 
 	public static void main(String[] args) {
-		// nothing to do here... everything runs from the enhancer.java class.
+		// Vi skal ikke bruge den her til noget, alt køres fra enhancer.java
 	}
 }
